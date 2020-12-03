aabd1cac7202204bd5dd6bdeb10a5f49767fb642
sk0752@unt.edu <sk0752@unt.edu>
2020-12-03 07:38:14 -0600

    Website rough

--- DjangoSK/src/mysite/settings.py
+++ DjangoSK/src/mysite/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"], 
+        'DIRS': [BASE_DIR / "templates", "static"], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
